a53bb417fc5baa80383b622945f1d672a485e377
ArceusTech <104206888+IlikeScript1234@users.noreply.github.com>
2022-09-12 22:06:48 +0700

    Update source.lua

--- Kavo-Ui-Library/main/source.lua
+++ Kavo-Ui-Library/main/source.lua
@@ -77,18 +77,18 @@ local themeStyles = {
         TextColor = Color3.fromRGB(255,255,255), 
         ElementColor = Color3.fromRGB(20, 20,2)
     Laserware = {
-        SchemeColor = Color3.fromRGB(255, 212,0), 
+        SchemeColor = Color3.fromRGB(255, 93,93), 
         Background = Color3.fromRGB(0, 0, 0), 
-        Header = Color3.fromRGB(168, 168, 168), 
+        Header = Color3.fromRGB(5, 5, 5), 
         TextColor = Color3.fromRGB(138,138,138), 
-        ElementColor = Color3.fromRGB(20, 20,20) 
+        ElementColor = Color3.fromRGB(20, 20,2) 
       },
     BloodTheme = {
         SchemeColor = Color3.fromRGB(227, 27, 27),
         Background = Color3.fromRGB(10, 10, 10),
         Header = Color3.fromRGB(5, 5, 5),
         TextColor = Color3.fromRGB(255,255,255),
-        ElementColor = Color3.fromRGB(20, 20,20)
+        ElementColor = Color3.fromRGB(20, 20, 20)
     },
     GrapeTheme = {
         SchemeColor = Color3.fromRGB(166, 71, 214),
